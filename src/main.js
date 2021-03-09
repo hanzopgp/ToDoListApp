e4ffd56bb0a102f3b597461cf7ac1a61d6cab7d4
hanzo <enzodurandcontact@gmail.com>
2021-03-09 23:10:21 +0100

    first commits app/main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import { store } from "./store/index";
 
-createApp(App).mount('#app')
+const app = createApp(App); 
+
+app.config.globalProperties.axios = axios; 
+  
+app.use(store).mount("#app");       
---
+++ src/view/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <todo-list />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from '../components/TodoList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TodoList
+  },
+
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
